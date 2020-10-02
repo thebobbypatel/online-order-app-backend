d595addfefa5f1247a35a8e73bb062ab6166dec8
Bobby Patel <bobbyrp@uci.edu>
2020-10-02 04:47:48 -0700

    uibuild

---
+++ build/precache-manifest.495b2e971df04628efaa9d78e89f8b4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3253fb63a9db6fb74b4fde4a3b64d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2f94cc13520527075e",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "6f8b0c9313eef710bf25",
+    "url": "/static/css/main.a133102b.chunk.css"
+  },
+  {
+    "revision": "0b2f94cc13520527075e",
+    "url": "/static/js/2.479241de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.479241de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8b0c9313eef710bf25",
+    "url": "/static/js/main.66b4aa23.chunk.js"
+  },
+  {
+    "revision": "8453747afe2591da8fb8",
+    "url": "/static/js/runtime-main.dce680fe.js"
+  }
+]);
