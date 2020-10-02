749df585c151571e89f1581d2119f2350cbcc8c0
Bobby Patel <bobbyrp@uci.edu>
2020-10-02 04:50:50 -0700

    uibuild

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 
-// const PORT = process.env.PORT
-// let MONGODB_URI = process.env.MONGODB_URI
-const PORT = 3001
-let MONGODB_URI = 'mongodb+srv://admin:admin@cluster0.uqj2d.mongodb.net/orders?retryWrites=true&w=majority'
+const PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
     MONGODB_URI = process.env.TEST_MONGODB_URI
